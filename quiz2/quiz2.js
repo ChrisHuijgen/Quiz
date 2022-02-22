fce93deb2d3b45dbce67b8392400fe5032528129
ChrisHuijgen <87247@glr.nl>
2022-02-22 12:27:34 +0100

    halllo

---
+++ quiz2/quiz2.js
@@ -0,0 +1,9 @@
+function volgende0() {
+document.getElementById("uitleg").style.display = "none";
+document.getElementById("vraag1").style.display = "block";
+  }
+
+  function vraag1() {
+    document.getElementById("vraag1").style.display = "none";
+    document.getElementById("vraag2").style.display = "block";
+      }
