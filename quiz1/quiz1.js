61637ef9cfac1eaeccda92cc1e53e677baefc330
stu87955 <87955@glr.nl>
2022-02-22 12:26:03 +0100

    commit#4

---
+++ quiz1/quiz1.js
@@ -0,0 +1,9 @@
+ function vraag1() {
+  document.getElementById("vraagEenf").style.display = "none";
+  document.getElementById("vraag2f").style.display = "block";
+}
+
+function vraag2() {
+ document.getElementById("vraag2f").style.display = "none";
+ document.getElementById("vraag3f").style.display = "block";
+}
