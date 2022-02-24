637ecacdca00b630e68eaf9286d86e223a5e3ab6
ChrisHuijgen <87247@glr.nl>
2022-02-24 10:02:58 +0100

    update

--- quiz2/quiz2.js
+++ quiz2/quiz2.js
@@ -7,3 +7,48 @@ document.getElementById("vraag1").style.display = "block";
     document.getElementById("vraag1").style.display = "none";
     document.getElementById("vraag2").style.display = "block";
       }
+
+      function vraag2() {
+        document.getElementById("vraag2").style.display = "none";
+        document.getElementById("vraag3").style.display = "block";
+          }
+        
+          function vraag3() {
+            document.getElementById("vraag3").style.display = "none";
+            document.getElementById("vraag4").style.display = "block";
+              }
+              
+              function vraag4() {
+                document.getElementById("vraag4").style.display = "none";
+                document.getElementById("vraag5").style.display = "block";
+                  }
+
+                  function vraag5() {
+                    document.getElementById("vraag5").style.display = "none";
+                    document.getElementById("vraag6").style.display = "block";
+                      }
+
+                      function vraag6() {
+                        document.getElementById("vraag6").style.display = "none";
+                        document.getElementById("vraag7").style.display = "block";
+                          }
+
+                          function vraag7() {
+                            document.getElementById("vraag7").style.display = "none";
+                            document.getElementById("vraag8").style.display = "block";
+                              }
+
+                              function vraag8() {
+                                document.getElementById("vraag8").style.display = "none";
+                                document.getElementById("vraag9").style.display = "block";
+                                  }
+
+                                  function vraag9() {
+                                    document.getElementById("vraag9").style.display = "none";
+                                    document.getElementById("vraag10").style.display = "block";
+                                      }
+
+                                      function vraag10() {
+                                        document.getElementById("vraag10").style.display = "none";
+                                        document.getElementById("resultaat").style.display = "block";
+                                          }
