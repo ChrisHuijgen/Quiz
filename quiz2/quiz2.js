4dc23820e70604bcdf9c434365c8bbcaf7b03b52
ChrisHuijgen <87247@glr.nl>
2022-03-08 12:24:59 +0100

    da

--- quiz2/quiz2.js
+++ quiz2/quiz2.js
@@ -1,4 +1,4 @@
-let punten = 0
+
 
 function volgende0() {
 document.getElementById("uitleg").style.display = "none";
@@ -55,10 +55,56 @@ document.getElementById("vraag7").style.display = "block";
 document.getElementById("resultaat").style.display = "block";
 }
 
+let punten = 0
 
-if(vraag1 = 'c'){
-punten =+ 10;
+if(vraag1 == 'c'){
+  punten = punten + 10;
 }
+if(vraag2 == 'b'){
+  punten = punten + 10;
+}
+if(vraag3 == 'a'){
+  punten = punten + 10;
+}
+if(vraag4 == 'c'){
+  punten = punten + 10;
+}
+if(vraag5 == 'b'){
+  punten = punten + 10;
+}
+if(vraag6 == 'a'){
+  punten = punten + 10;
+}
+if(vraag7 == 'a'){
+  punten = punten + 10;
+}
+if(vraag8 == 'a'){
+  punten = punten + 10;
+}
+if(vraag9 == 'c'){
+  punten = punten + 10;
+}
+if(vraag10 == 'b'){
+  punten = punten + 10;
+}
+
+function resultaat(){
+  if (punten <= 50){
+  document.write("Gefeliciteerd je hebt de quiz afgemaakt!")
+  document.write("Je hebt " + punten + "gehaald")
+  }
+  if (punten > 60){
+    document.write("Gefeliciteerd je hebt de quiz afgemaakt!")
+    document.write("Je hebt " + punten + "gehaald")
+  }
+}
+
+
+
+
+
+
+
 
 
  
