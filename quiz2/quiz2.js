67a53522125410120a85fa8f2e0379cbb96b4a69
ChrisHuijgen <87247@glr.nl>
2022-03-15 10:03:02 +0100

    eindelijk klaar

--- quiz2/quiz2.js
+++ quiz2/quiz2.js
@@ -1,120 +1,143 @@
-
+punten = 0
 
 function volgende0() {
-document.getElementById("uitleg").style.display = "none";
-document.getElementById("vraag1").style.display = "block";
+  document.getElementById("uitleg").style.display = "none";
+  document.getElementById("vraag1").style.display = "block";
+}
+
+function vraag1(antwoord) {
+  if (antwoord == 'c') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag1").style.display = "none";
+    document.getElementById("vraag2").style.display = "block";
   }
-
-  function vraag1() {
-  document.getElementById("vraag1").style.display = "none";
-  document.getElementById("vraag2").style.display = "block";
-  }
-
- function vraag2() {
-   document.getElementById("vraag2").style.display = "none";
-document.getElementById("vraag3").style.display = "block";
-          }
-        
-function vraag3() {
-document.getElementById("vraag3").style.display = "none";
-document.getElementById("vraag4").style.display = "block";
-}
-              
-function vraag4() {
-   document.getElementById("vraag4").style.display = "none";
-  document.getElementById("vraag5").style.display = "block";
-  }
-
- function vraag5() {
-  document.getElementById("vraag5").style.display = "none";
-   document.getElementById("vraag6").style.display = "block";
-  }
-
- function vraag6() {
- document.getElementById("vraag6").style.display = "none";
-document.getElementById("vraag7").style.display = "block";
-  }
-
-  function vraag7() {
- document.getElementById("vraag7").style.display = "none";
- document.getElementById("vraag8").style.display = "block";
- }
-
- function vraag8() {
-  document.getElementById("vraag8").style.display = "none";
-  document.getElementById("vraag9").style.display = "block";
- }
-
-  function vraag9() {
-  document.getElementById("vraag9").style.display = "none";
-  document.getElementById("vraag10").style.display = "block";
-  }
-
- function vraag10() {
- document.getElementById("vraag10").style.display = "none";
-document.getElementById("resultaat").style.display = "block";
-}
-
-
-var punten = 0
-
-if(vraag1 == 'c'){
- var punten = punten + 10;
-  console.log("punten")
-}
-if(vraag2 == 'b'){
- var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag3 == 'a'){
-  var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag4 == 'c'){
- var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag5 == 'b'){
- var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag6 == 'a'){
-  var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag7 == 'a'){
-  var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag8 == 'a'){
-  var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag9 == 'c'){
- var punten = punten + 10;
-  console.log(punten)
-}
-if(vraag10 == 'b'){
- var punten = punten + 10;
-  console.log(punten)
-}
-
-function resultaat(){
-  var punten = 60
-  if (punten <= 50){
-  document.write("Gefeliciteerd je hebt de quiz afgemaakt!")
-  var imghoog = new Image(400, 300);
-  imghoog.src = 'foto/dkhoog.jpg';
-  document.write("Je hebt " + punten + "gehaald")
-  }
-  if (punten >= 60){
-    document.write("Gefeliciteerd je hebt de quiz afgemaakt!")
-    var imglaag = new Image(400, 300);
-    imglaag.src = 'foto/dklaag.jpg';
-    document.write("Je hebt " + punten + "gehaald")
+  else if (antwoord == "a" || antwoord == "b") {
+    document.getElementById("vraag1").style.display = "none";
+    document.getElementById("vraag2").style.display = "block";
   }
 }
 
+function vraag2(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag2").style.display = "none";
+    document.getElementById("vraag3").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag2").style.display = "none";
+    document.getElementById("vraag3").style.display = "block";
+  }
+}
+
+function vraag3(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag3").style.display = "none";
+    document.getElementById("vraag4").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag3").style.display = "none";
+    document.getElementById("vraag4").style.display = "block";
+  }
+}
+
+function vraag4(antwoord) {
+  if (antwoord == 'c') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag4").style.display = "none";
+    document.getElementById("vraag5").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "b") {
+    document.getElementById("vraag4").style.display = "none";
+    document.getElementById("vraag5").style.display = "block";
+  }
+}
+
+function vraag5(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag5").style.display = "none";
+    document.getElementById("vraag6").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag5").style.display = "none";
+    document.getElementById("vraag6").style.display = "block";
+  }
+}
+
+function vraag6(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag6").style.display = "none";
+    document.getElementById("vraag7").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag6").style.display = "none";
+    document.getElementById("vraag7").style.display = "block";
+  }
+}
+
+function vraag7(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag7").style.display = "none";
+    document.getElementById("vraag8").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag7").style.display = "none";
+    document.getElementById("vraag8").style.display = "block";
+  }
+}
+
+function vraag8(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag8").style.display = "none";
+    document.getElementById("vraag9").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag8").style.display = "none";
+    document.getElementById("vraag9").style.display = "block";
+  }
+}
+
+function vraag9(antwoord) {
+  if (antwoord == 'c') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag9").style.display = "none";
+    document.getElementById("vraag10").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "b") {
+    document.getElementById("vraag9").style.display = "none";
+    document.getElementById("vraag10").style.display = "block";
+  }
+}
+
+function vraag10(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag10").style.display = "none";
+    document.getElementById("punten").innerHTML = "Je heeft " + punten + "/100 gehaald!"
+    document.getElementById("resultaat").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag10").style.display = "none";
+    document.getElementById("punten").innerHTML = "Je heeft " + punten + "/100 gehaald!"
+    document.getElementById("resultaat").style.display = "block";
+
+  }
+
+}
 
 
 
@@ -123,4 +146,5 @@ function resultaat(){
 
 
 
- 
+
+
