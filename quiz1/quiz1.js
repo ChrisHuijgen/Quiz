90ce64d6cd1c04a71c7a4d51991ec02d3dea72f1
stu87955 <87955@grl.nl>
2022-03-17 11:10:26 +0100

    commit#7

--- quiz1/quiz1.js
+++ quiz1/quiz1.js
@@ -1,49 +1,133 @@
-function vraag1() {
-  document.getElementById("vraagEenf").style.display = "none";
-  document.getElementById("vraag2f").style.display = "block";
+punten = 0
+
+function vraag1(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraagEenf").style.display = "none";
+    document.getElementById("vraag2f").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraagEenf").style.display = "none";
+    document.getElementById("vraag2f").style.display = "block";
+  }
 }
 
-function vraag2() {
- document.getElementById("vraag2f").style.display = "none";
- document.getElementById("vraag3f").style.display = "block";
+function vraag2(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag2f").style.display = "none";
+    document.getElementById("vraag3f").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag2f").style.display = "none";
+    document.getElementById("vraag3f").style.display = "block";
+  }
 }
 
-function vraag3() {
-  document.getElementById("vraag3f").style.display = "none";
-  document.getElementById("vraag4f").style.display = "block";
+function vraag3(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag3f").style.display = "none";
+    document.getElementById("vraag4f").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag3f").style.display = "none";
+    document.getElementById("vraag4f").style.display = "block";
+  }
  }
 
- function vraag4() {
-  document.getElementById("vraag4f").style.display = "none";
-  document.getElementById("vraag5f").style.display = "block";
+ function vraag4(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag4f").style.display = "none";
+    document.getElementById("vraag5f").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag4f").style.display = "none";
+    document.getElementById("vraag5f").style.display = "block";
+  }
  }
 
- function vraag5() {
-  document.getElementById("vraag5f").style.display = "none";
-  document.getElementById("vraag6f").style.display = "block";
+ function vraag5(antwoord) {
+  if (antwoord == 'c') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag5f").style.display = "none";
+    document.getElementById("vraag6f").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "b") {
+    document.getElementById("vraag5f").style.display = "none";
+    document.getElementById("vraag6f").style.display = "block";
+  }
  }
 
- function vraag6() {
-  document.getElementById("vraag6f").style.display = "none";
-  document.getElementById("vraag7f").style.display = "block";
+ function vraag6(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag6f").style.display = "none";
+    document.getElementById("vraag7f").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag6f").style.display = "none";
+    document.getElementById("vraag7f").style.display = "block";
+  }
  }
 
- function vraag7() {
-  document.getElementById("vraag7f").style.display = "none";
-  document.getElementById("vraag8f").style.display = "block";
+ function vraag7(antwoord) {
+  if (antwoord == 'c') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag7f").style.display = "none";
+    document.getElementById("vraag8f").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "b") {
+    document.getElementById("vraag7f").style.display = "none";
+    document.getElementById("vraag8f").style.display = "block";
+  }
  }
 
- function vraag8() {
-  document.getElementById("vraag8f").style.display = "none";
-  document.getElementById("vraag9f").style.display = "block";
+ function vraag8(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag8f").style.display = "none";
+    document.getElementById("vraag9f").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag8f").style.display = "none";
+    document.getElementById("vraag9f").style.display = "block";
+  }
  }
 
- function vraag9() {
-  document.getElementById("vraag9f").style.display = "none";
-  document.getElementById("vraag10f").style.display = "block";
+ function vraag9(antwoord) {
+  if (antwoord == 'a') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag9f").style.display = "none";
+    document.getElementById("vraag10f").style.display = "block";
+  }
+  else if (antwoord == "b" || antwoord == "c") {
+    document.getElementById("vraag9f").style.display = "none";
+    document.getElementById("vraag10f").style.display = "block";
+  }
  }
 
- function vraag10() {
-  document.getElementById("vraag10f").style.display = "none";
-  document.getElementById("vraagf").style.display = "block";
+ function vraag10(antwoord) {
+  if (antwoord == 'b') {
+    punten = punten + 10;
+    console.log(punten)
+    document.getElementById("vraag10f").style.display = "none";
+    document.getElementById("punten").innerHTML = "Je heeft " + punten + "/100 gehaald!"
+    document.getElementById("vraageind").style.display = "block";
+  }
+  else if (antwoord == "a" || antwoord == "c") {
+    document.getElementById("vraag10f").style.display = "none";
+    document.getElementById("punten").innerHTML = "Je heeft " + punten + "/100 gehaald!"
+    document.getElementById("vraageind").style.display = "block";
+  }
  }
